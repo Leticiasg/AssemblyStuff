e34e9575bf1fb3b0f7b6027d155d515e600498de
schumacker <ev.schumacker@gmail.com>
2012-11-10 02:43:42 -0200

    continuando a merda dessa porra dessa bosta de escalonador

--- Scheduler/SOS/scheduler.s
+++ Scheduler/SOS/scheduler.s
@@ -11,14 +11,29 @@ scheduler:
   ldmfd sp!, {r0-r12, lr}
 
 
-_salva_contexto:
+_save_context:
   ldr r1, =usr_registers
-  ldr r2, =current_pid
-  ldr r2, [r2]
-  ldr r1, [r1, r2] @ r1 = apontador para o vetor de registers do user
+  ldr r2, =svc_registers
+  ldr r3, =current_pid
+  ldr r3, [r3]
+  ldr r1, [r1, r3] @ r1 = apontador para o vetor de registers do user
+  ldr r2, [r2, r3] @ r2 = apontador para o veotr de registers do supervisor
+  eor r4, r4, r4   @ contador
+
+__laco_save_usr_registers:      
+  ldr r5, [r0], #4
+  str r5, [r1], #4 @ grava r0-r12 no vetor de registers de user
+  str r5, [r2], #4 @ grava r0-r12 no vetor de registers de supervisor
+  add r4, r4, #1
+  cmp r4, #13
+  blt __laco_save_usr_registers
+
+  msr cpsr_c, #0xd3 @ muda para o modo supervisor, desabilitando interrupcoes
+  
+
+  mov pc, lr
   
         
-
 @-----------------------------------@
 @                                   @
 @            variaveis              @
