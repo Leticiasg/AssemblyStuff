2999ea1f97f1e9bef95f92de70ba0b19c65bf685
schumacker <ev.schumacker@gmail.com>
2012-11-10 15:10:01 -0200

    agora sim a salva de contexto ta sem problemas

--- Scheduler/SOS/scheduler.s
+++ Scheduler/SOS/scheduler.s
@@ -21,29 +21,24 @@ _save_context:
   eor r4, r4, r4   @ contador
 
 __laco_save_usr_registers:      
-  ldr r5, [r0!], #4
-  str r5, [r1!], #4 @ grava r0-r12 no vetor de registers de user
-  str r5, [r2!], #4 @ grava r0-r12 no vetor de registers de supervisor
+  ldr r5, [r0], #4
+  str r5, [r1], #4 @ grava r0-r12 no vetor de registers de user
+  str r5, [r2], #4 @ grava r0-r12 no vetor de registers de supervisor
   add r4, r4, #1
   cmp r4, #13
   blt __laco_save_usr_registers
 
-  mov r0, spsr @ Pega as flags de cpsr
-  str r0, [r1, #16] @ salva cpsr na posicao 16 do vetor de usr_registers
-  str r0, [r2, #16] @ salva cpsr na posicao 16 do vetor de svc_registers
-
   @ muda para o modo supervisor, desabilitando interrupcoes      
   msr cpsr_c, #0xd3 
-  str r13, [r2!], #4
-  str r14, [r2!], #4
-  str r15, [r2!], #4
-  str spsr, [r2, #8]
+  str r13, [r2], #4
+  str r14, [r2], #4
+  str r15, [r2], #4
 
   @ muda para o modo supervisor, desabilitando interrupcoes
   msr CPSR_c, #0xDF
-  str r13, [r1!], #4
-  str r14, [r1!], #4
-  str r15, [r1!], #4
+  str r13, [r1], #4
+  str r14, [r1], #4
+  str r15, [r1], #4
 
   mov pc, lr
   
