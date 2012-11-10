bf2abef0f98d319fbeecaa374d533a97a2636614
tlgimenes <tlgimenes@gmail.com>
2012-11-10 02:03:06 -0200

    Syscalls FINISHED but not debugged !!!

--- Scheduler/SOS/exit.s
+++ Scheduler/SOS/exit.s
@@ -13,24 +13,13 @@
   .align 4
   .global Sos_exit
 
+@ Arguments: r0 = status
+@ Return: NONE
 Sos_exit:
   stmfd sp!, {r4-r11, lr}
 
-@ Get current process running
-@ index of the process is in r4
-  ldr r7, =RUNNING
-  ldr r5, =process_status
-  mov r4, #-1
-__get_running:
-  add r4, r4 ,#1
-  cmp r4, #8
-  beq __return_Sos_exit   @ If no active process found
-  ldrb r6, [r5, r4]
-  cmp r6, r7              @ Compares r6 with RUNNING value
-  bne __get_running
-  
-@ Save r5 in tmp register
-  mov r12, r5
+  stmfd sp!, {r0}
+  bl _get_running_process
 
 @ Clean registers
   mov r7, #0
@@ -57,9 +46,33 @@ __fill_usr_registers:
   str r9, [r8, r5]        @ Stor usr_stack
 
 @ Free position of process
+  ldr r5, =process_status
   ldr r6, =WAITING
-  strb r6, [r12, r4]
+  strb r6, [r5, r4]
 
 __return_Sos_exit:
   ldmfd sp!, {r4-r11, lr}
   movs pc, lr
+
+@------------------------------------------------@
+
+@ Arguments: NONE
+@ Return: r0 = index of the running process
+_get_running_process:
+  stmfd sp!, {r4-r11, lr}
+
+@ Get current process running
+@ index of the process is in r4
+  ldr r7, =RUNNING
+  ldr r5, =process_status
+  mov r0, #-1
+__get_running:
+  add r0, r0 ,#1
+  cmp r0, #8                @ 8 is the max process number
+  beq __return_running      @ If no active process found
+  ldrb r6, [r5, r0]
+  cmp r6, r7                @ Compares r6 with RUNNING value
+  bne __get_running
+
+__return_running:
+  ldmfd sp!, {r4-r11, pc}
--- Scheduler/SOS/fork.s
+++ Scheduler/SOS/fork.s
@@ -13,26 +13,97 @@
   .align 4
   .global Sos_fork
 
+@ Arguments: NONE
+@ Return: child : r0 = 0 if sucess
+@          parent: r0 = child's PID if sucess -1 if fail
 Sos_fork:
   stmfd sp!, {r4-r11, lr}
 
 @ Get current process running
 @ index of the process is in r4
-  ldr r7, =RUNNING
-  ldr r5, =process_status
-  mov r4, #-1
-__get_running_pid:
-  add r4, r4 ,#1
-  cmp r4, #8                @ 8 is the max process number
-  beq __return_Sos_getpid   @ If no active process found
-  ldrb r6, [r5, r4]
-  cmp r6, r7                @ Compares r6 with RUNNING value
-  bne __get_running_pid
- 
+  mov r0, #0
+  bl _get_running_process
+  mov r1, r0
 
+@ Get next index of available PID
+  bl _get_next_available
+  cmp r0, #-1
+  beq __return_Sos_fork
 
+@ If achieved this point, it is possible to create a new
+@ child process
+
+@ copy the exact context from parent to child process
+  bl _copy_context
+
+@ change the pc for child process
+  ldr r5, =__child_return
+  ldr r4, =usr_registers
+  ldr r4, [r4, r0]
+  str r5, [r4, #15]
+
+__return_Sos_fork:
+  add r0, r0, #1            @ PID is in r0
   ldmfd sp!, {r4-r11, lr}
   movs pc, lr
+
+__child_return:
+  mov r0, #0
+  ldmfd sp!, {r4, r11, lr}
+  movs pc, lr
+
+@------------------------------------------------@
+
+@ Arguments: r0 = index of child process
+@            r1 = index of parent process
+@ Return: NONE
+_copy_context:
+  stmfd sp!, {r4-r11, lr}
+
+@ Get address of contexts of parent and child process
+  ldr r4, =usr_registers
+  ldr r6, [r4, r0]        @ Address of child  user context
+  ldr r4, [r4, r1]        @ Address of parent user context
+  ldr r5, =svc_registers
+  ldr r7, [r5, r0]        @ Adress of child  svc context
+  ldr r5, [r5, r1]        @ Adress of parent svc context
+
+  mov r9, #0              @ Iterator
+__loop_copy_context:
+  ldr r8, [r4, r9]        @ Copy parent user
+  str r8, [r6, r9]        @ to child user register
+  ldr r8, [r5, r9]        @ Copy parent svc register
+  str r8, [r7, r9]        @ to child svc register
+  cmp r9, #68
+  bne __loop_copy_context
+  ldr r8, [r5, r9]        @ Copy parent spsr
+  str r8, [r7, r9]        @ to child spsr
+
+  ldmfd sp!, {r4-r11, pc}
+
+@------------------------------------------------@
+
+@ Arguments: NONE
+@ Return: r0 = next available if sucess -1 if not available
+_get_next_available:
+  stmfd sp!, {r4-r11, lr}
+
+  ldr r4, =WAITING
+  ldr r5, =process_status
+  mov r0, #-1
+  mov r6, #-1
+__get_available:
+  add r6, r6, #1
+  cmp r6, #8                          @ 8 is the max process number
+  beq __return_get_next_available     @ if no available PID found
+  ldrb r7, [r5, r6]
+  cmp r7, r4
+  bne __get_available
+
+  mov r0, r6
+
+__return_get_next_available:
+  ldmfd sp!, {r4-r11, pc}
 
 @------------------------------------------------@
 
